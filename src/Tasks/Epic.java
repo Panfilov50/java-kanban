c0a3fce4aa67477725630b9a9f4d83341604a0ec
Panfilov50 <117942994+Panfilov50@users.noreply.github.com>
2022-12-18 00:14:44 +0300

    Add files via upload

---
+++ src/Tasks/Epic.java
@@ -0,0 +1,10 @@
+package Tasks;
+import Manager.*;
+
+public class Epic extends Task {
+    public Epic(int id, String name, String description, Status status) {
+        super(id, name, description, status);
+    }
+
+
+}
